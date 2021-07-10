38600d552014713c755dc4b2126f4903ed5d7455
Mikey Christoferis Qu4rkz <71943288+Christoferis@users.noreply.github.com>
2021-07-10 10:27:43 +0200

    automatic Tab resizing

--- tab.py
+++ tab.py
@@ -7,8 +7,11 @@ class Tab:
 
     def __init__(self, **kwargs) -> None:
         #open a window
-        with dpg.window(label="test") as self.win:
-            txt_widget = dpg.add_input_text()
+        with dpg.window(label="test", width=500, height=500) as self.win:
+            
+            #create resizing callback
+            self.txt_widget = dpg.add_input_text(multiline=True, tab_input=True)
+            dpg.add_resize_handler(self.win, callback=self.resize)
 
         #kwargs for savepath, input text and so on
         #input
@@ -16,7 +19,6 @@ class Tab:
             self.input = kwargs["savepath"]
         except KeyError:
             self.input = None
-
         pass
 
 
@@ -31,5 +33,13 @@ class Tab:
         path = self.savepath
         pass
 
+    #input txt directly into window
     def tab_input(self, input):
+        dpg.set_value()
+        pass
+
+    #resize widget according to window
+    def resize(self, sender):
+        dpg.set_item_height(self.txt_widget, dpg.get_item_height(self.win))
+        dpg.set_item_width(self.txt_widget, dpg.get_item_width(self.win))
         pass
